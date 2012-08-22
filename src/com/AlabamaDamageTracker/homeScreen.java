86fb72e0d30d9f2a5d22ec258a4bee575d5957f1
clhodapp <clhodapp1@gmail.com>
2012-08-22 12:37:06 -0500

    homeScreen: fix indention

--- src/com/AlabamaDamageTracker/homeScreen.java
+++ src/com/AlabamaDamageTracker/homeScreen.java
@@ -15,107 +15,97 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class homeScreen extends Activity {
-	
+
 	private LocationManager locationManager;
 	private LocationListener locationListener;
 	protected final Context Context = this;
 	private long LocationID;
 	private String latitude = null;
 	private String longitude = null;
-	
-    /** Called when the activity is first created. */
-    @Override
-	   public void onCreate(Bundle savedInstanceState) {
-	      super.onCreate(savedInstanceState);
-	      
-	   }
-	   
-	   public void onResume(){
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	public void onResume() {
 		super.onResume();
-        setContentView(R.layout.home_screen);
-    
-        
-        // Get access to the system location services. 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);  
-        
-        // Set up a location listener for the device.
-        locationListener = new MyLocationListener();
-        
-        // Receive GPS location updates for the device.
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        
-        
-        
-        final Button newDamage = (Button) findViewById(R.id.home_new_location);
-        final Button reportedDamage = (Button) findViewById(R.id.home_reported_damage);
+		setContentView(R.layout.home_screen);
 
-        
-        currentDamage info = ((currentDamage)getApplicationContext());
-	    LocationID = info.getLocationID();
-        
-        newDamage.setOnClickListener (new View.OnClickListener() {
-		   	public void onClick(View v) {
-		   		databaseHelper myDbHelper = new databaseHelper(Context);    
-		        myDbHelper.openDataBase();
-		   		long locationID = myDbHelper.insertDamage(latitude, longitude, "", "", "", "", "", "", "");
-		   		((currentDamage) Context.getApplicationContext()).setLocationID(locationID);
-		   		//myDbHelper.updateGPS(latitude, LocationID, longitude);
+		// Get access to the system location services. 
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);  
+
+		// Set up a location listener for the device.
+		locationListener = new MyLocationListener();
+
+		// Receive GPS location updates for the device.
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+
+
+		final Button newDamage = (Button) findViewById(R.id.home_new_location);
+		final Button reportedDamage = (Button) findViewById(R.id.home_reported_damage);
+
+
+		currentDamage info = ((currentDamage)getApplicationContext());
+		LocationID = info.getLocationID();
+
+		newDamage.setOnClickListener (new View.OnClickListener() {
+			public void onClick(View v) {
+				databaseHelper myDbHelper = new databaseHelper(Context);    
+				myDbHelper.openDataBase();
+				long locationID = myDbHelper.insertDamage(latitude, longitude, "", "", "", "", "", "", "");
+				((currentDamage) Context.getApplicationContext()).setLocationID(locationID);
+				//myDbHelper.updateGPS(latitude, LocationID, longitude);
 				myDbHelper.close();
-		   		Intent intent = new Intent(getBaseContext(),takePicture_screen.class);
-	   			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-	   		    startActivityForResult(intent,0);
-		   	}
-        });
-        
-        reportedDamage.setOnClickListener (new View.OnClickListener() {
-		   	public void onClick(View v) {
-	   			Intent intent = new Intent(getBaseContext(),selectDamage_screen.class);
-	   			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-	   		    startActivityForResult(intent,0);
-		   	}
-        });
-        
-    
-    }
-    
-    public class MyLocationListener implements LocationListener
+				Intent intent = new Intent(getBaseContext(),takePicture_screen.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivityForResult(intent,0);
+			}
+		});
 
-	{
+		reportedDamage.setOnClickListener (new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(getBaseContext(),selectDamage_screen.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivityForResult(intent,0);
+			}
+		});
 
-	public void onLocationChanged(Location loc)
-	{
-	latitude = String.valueOf(loc.getLatitude());
-	longitude = String.valueOf(loc.getLongitude());
-	}
-
-
-	public void onProviderDisabled(String provider)
-
-	{
-
-	Toast.makeText( getApplicationContext(),
-
-	"Gps Disabled",
-
-	Toast.LENGTH_SHORT ).show();
 
 	}
 
+	public class MyLocationListener implements LocationListener {
 
-	public void onProviderEnabled(String provider)
+		public void onLocationChanged(Location loc) {
+			latitude = String.valueOf(loc.getLatitude());
+			longitude = String.valueOf(loc.getLongitude());
+		}
 
-	{
 
-	Toast.makeText( getApplicationContext(),
-	"Gps Enabled",
-	Toast.LENGTH_SHORT).show();
+		public void onProviderDisabled(String provider) {
+
+			Toast.makeText( getApplicationContext(),
+
+					"Gps Disabled",
+
+					Toast.LENGTH_SHORT ).show();
+
+		}
+
+
+		public void onProviderEnabled(String provider) {
+			Toast.makeText( getApplicationContext(),
+					"Gps Enabled",
+					Toast.LENGTH_SHORT).show();
+		}
+
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+
 	}
 
 
-	public void onStatusChanged(String provider, int status, Bundle extras)
-	{
-	}
-	}
-	
-    
 }
