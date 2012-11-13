ddfc8ec2f03990888a2e34b66568e7bce7571134
clhodapp <clhodapp1@gmail.com>
2012-11-12 23:16:47 -0600

    Start work on UploadService

---
+++ app/src/com/AlabamaDamageTracker/UploadService.java
@@ -0,0 +1,47 @@
+package com.AlabamaDamageTracker;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+public class UploadService extends IntentService {
+	
+	public static final String KEY_REPORT_ID = "report_id";
+	public static final String KEY_USERNAME = "username";
+	public static final String KEY_PASSWORD = "password";
+	
+	private Context self = this;
+	
+	private class UploadImageTask extends AsyncTask {
+
+		@Override
+		protected Object doInBackground(Object... params) {
+			return null;
+		}
+		
+	}
+
+	public UploadService() {
+		super("UplaodServiceWorker");
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		
+		long reportId = intent.getLongExtra(KEY_REPORT_ID, -1l);
+		String username = intent.getStringExtra(KEY_USERNAME);
+		String password = intent.getStringExtra(KEY_PASSWORD);
+		
+		DatabaseHelper dbh = DatabaseHelper.openReadOnly(self);
+		try {
+			Report source = dbh.getReport(reportId);
+		} finally {
+			dbh.close();
+		}
+
+		
+		
+	}
+
+}
